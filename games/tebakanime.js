261a88a9d0afdfd182174e229577c4846f5593ce
XputsDev <ap9009518@gmail.com>
2022-11-17 17:08:31 +0700

    Mentahan Bot

---
+++ games/tebakanime.js
@@ -0,0 +1,302 @@
+[{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/8eh5vvj9",
+        "jawaban": "Himiko Toga"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/2tmrtdrw",
+        "jawaban": "Hitagi Senjougahara"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/3kfpv48z",
+        "jawaban": "Nano Shinonome"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/2p9d9zv6",
+        "jawaban": "Kurisu Makise"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/bdcnhu52",
+        "jawaban": "Azusa Azuki"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/38psff75",
+        "jawaban": "Holo"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/bp7t5ces",
+        "jawaban": "Marisa Kirisame"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/bdhvy4u2",
+        "jawaban": "Rem"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/2f3ckznv",
+        "jawaban": "Tobiichi Origami"
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/4d7kbv3h",
+        "jawaban": "Rei Ayanami"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/2s37uaty",
+        "jawaban": "Shuchi"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/5b984ehm",
+        "jawaban": "Ranko Kanzaki"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/6k98uwha",
+        "jawaban": "Manami Tamura"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/2p9ftdnp",
+        "jawaban": "Shinobu Oshino"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/5h5bbp25",
+        "jawaban": "Yuno Gasai"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/mr24j94b",
+        "jawaban": "Satsuki Kiryuin"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/yw3btded",
+        "jawaban": "Shuten Douji"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y9w5t5mg",
+        "jawaban": "Himari Uehara"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y9vjp6df",
+        "jawaban": "Haruhi Suzumiya"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/ybv2uv49",
+        "jawaban": "Sena Kashiwazaki"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y9xydjrh",
+        "jawaban": "Ran Mintake"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y776oaww",
+        "jawaban": "Mai Sakurajima"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y9watmxg",
+        "jawaban": "Ochaco Uraraka"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/ybchtnqx",
+        "jawaban": "Yukiho Hagiwara"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y9rum5xp",
+        "jawaban": "Kurumi Tokisaki"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y8hf2zn3",
+        "jawaban": "Revy"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y6ux43ml",
+        "jawaban": "Raphtalia"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y73yvcgg",
+        "jawaban": "Madoka Kaname"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/yahpehw3",
+        "jawaban": "Kyouko Toshinou"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y883rap5",
+        "jawaban": "Saori Makishima"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/yckymfhv",
+        "jawaban": "Aqua"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y7q8wkdc",
+        "jawaban": "Yuri Nakamura"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y8nyazpy",
+        "jawaban": "Rikka Takarada"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y7jeh62l",
+        "jawaban": "Sakura Kinomoto"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y7p8redo",
+        "jawaban": "Ruri Gokou"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y7mcb6df",
+        "jawaban": "Albedo"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y8vljbj9",
+        "jawaban": "Rin Shibuya "        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y7675ov6",
+        "jawaban": "Zero Two"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "jawaban": "Princess King Boo",
+        "soalanime": "https://tinyurl.com/y9rvx33d"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y9xydjrh",
+        "jawaban": "Ran Mitake"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/ybxar4x2",
+        "jawaban": "Nadeshiko Kagamihara"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/ydy4wfwl",
+        "jawaban": "Chitoge Kirisaki"        
+    }
+},
+{
+    "result": {
+        "Created": "@lolhuman",
+        "soalanime": "https://tinyurl.com/y9zk45cz",
+        "jawaban": "Nano Shinonome"        
+    }
+}
+]
